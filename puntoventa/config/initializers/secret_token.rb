078bbe8dd2ba323707deb3fb8a0d44758a24a91e
conyhsaucedo <cony_860422@hotmail.com>
2014-02-27 19:23:52 -0600

    estructura del proyecto restaurantero

---
+++ puntoventa/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puntoventa::Application.config.secret_key_base = 'e6ebe73873c96b7db26bb7c41083d60ce612b4dfc76f01fe2b294ba1fe6b64e723bbcdf636fac9a50d1a25879a311aaab38fcf575f0c89b20b5d8fea32602893'
---
+++ puntoventa/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Puntoventa::Application.config.session_store :cookie_store, key: '_puntoventa_session'
